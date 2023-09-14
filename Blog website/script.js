c149ce0ec8e031ddc25508704c576a933879e35a
himanshi0820 <144801375+himanshi0820@users.noreply.github.com>
2023-09-14 22:58:44 +0530

    Add files via upload

---
+++ Blog website/script.js
@@ -0,0 +1,33 @@
+function first(){
+    document.getElementById("slideimage1").src="Images/nature1.jpg";
+}
+function second(){
+    document.getElementById("slideimage1").src="Images/nature2.png";
+}
+function third(){
+    document.getElementById("slideimage1").src="Images/nature3.jpg";
+}
+function fourth(){
+    document.getElementById("slideimage1").src="Images/woods.jpg";
+}
+setInterval(first, 2000);
+setInterval(second, 4000);
+setInterval(third, 6000);
+setInterval(fourth, 8000);
+
+function fifth(){
+    document.getElementById("slideimage2").src="Images/nature4.jfif";
+}
+function sixth(){
+    document.getElementById("slideimage2").src="Images/nature5.jfif";
+}
+function seventh(){
+    document.getElementById("slideimage2").src="Images/nature6.jfif";
+}
+function eighth(){
+    document.getElementById("slideimage2").src="Images/download.jfif";
+}
+setInterval(fifth, 2000);
+setInterval(sixth, 4000);
+setInterval(seventh, 6000);
+setInterval(eighth, 8000);
